d86a311c1bb1f56d3f43e2a238b22a3f82aab49f
Fhgdhgvhggvfgg <cars.fast120@gmail.com>
2026-07-21 19:26:57 +0300

    Last Sync: 2026-07-21 19:26 (Mobile)

--- scripts/dialogs.js
+++ scripts/dialogs.js
@@ -96,9 +96,9 @@ function showMyDialog() {
   const d = new BaseDialog("hello");
   
   // قم بتغيير الاستدعاء القديم إلى اسم صورتك الجديدة مع بادئة اسم المود:
-  d.cont.image(Core.atlas.find("cheetah-01-welcome_logo")).size(600, 300).pad(10).row();
+  d.cont.image(Core.atlas.find("cheetah-01-welcome_logo")).size(600, 600).pad(10).row();
   
-  d.cont.add("welcome to cheetah mod:)").row();
+  d.cont.add("welcome to cheetah mod.").row();
   d.buttons.button("bye", () => d.hide()).size(210, 64);
   d.show();
 }
